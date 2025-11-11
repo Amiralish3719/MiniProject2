2134baa03b3ecd520369b872b2a3c5c068156c50
Amirali Sheikh Hassani <amirali.sh3719@gmail.com>
2025-11-12 01:04:18 +0330

    Creaete Project and add Classes and Functions

---
+++ Dozz.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Player
+{
+private:
+    char PLCharacter;
+    string PLname;
+
+public:
+    Player(char PLChar, string pLname)
+    {
+        PLCharacter = PLChar;
+        PLname = pLname;
+    };
+};
+
+class Board
+{
+private:
+    char board;
+    int Counter;
+
+public:
+    void drawBoard() {
+
+    };
+
+    void isValidMove() {
+
+    };
+
+    void makeMove() {
+
+    };
+    void checkWin() {
+
+    };
+    void isFull() {
+
+    };
+};
+
+class Dooz
+{
+    void play() {
+
+    };
+};
+
+int main() {
+
+};
